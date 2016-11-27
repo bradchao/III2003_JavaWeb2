a53507533bcd1ebd8370c4a8a551f16dbd150421
user <user@Z97>
2016-11-27 11:45:17 +0800

    v3

--- JavaWeb/src/tw/org/iii/Hello.java
+++ JavaWeb/src/tw/org/iii/Hello.java
@@ -2,6 +2,8 @@ package tw.org.iii;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,8 +14,27 @@ public class Hello extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     public Hello() {
-    	System.out.println("Hello():v1");
+    	System.out.println("Hello()");
     }
+    
+    
+
+	@Override
+	public void init() throws ServletException {
+		System.out.println("init()");
+		//super.init();
+	}
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		System.out.println("service()");
+		//super.service(arg0, arg1);
+		
+		String key1 = request.getParameter("key1");
+		System.out.println("key1 = " + key1);
+		
+	}
+
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("doGet()");
