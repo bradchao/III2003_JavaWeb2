d2ec9fa19a151701444691a1a07687d1adab3573
user <user@Z97>
2016-12-03 13:31:52 +0800

    201612031331

--- JavaWeb/src/tw/org/iii/Brad07.java
+++ JavaWeb/src/tw/org/iii/Brad07.java
@@ -4,21 +4,23 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
+@MultipartConfig
 @WebServlet("/Brad07")
 public class Brad07 extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=utf-8");
 		PrintWriter out = response.getWriter();
 		
-		//Part part = request.getPart("upload");
+		Part part = request.getPart("upload");
 		
-		String header = request.getHeader("Content-Disposition"); //Content-Disposition
+		String header = part.getHeader("Content-Disposition"); //Content-Disposition
 		out.print(header + "<br>");
 		
 		
