b854ecab21064600b76276bc1c23c596de0f2dc6
user <user@Z97>
2016-12-03 15:10:17 +0800

    201612031510

--- JavaWeb/src/tw/org/iii/Brad09.java
+++ JavaWeb/src/tw/org/iii/Brad09.java
@@ -1,12 +1,9 @@
 package tw.org.iii;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Collection;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -15,20 +12,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
-@MultipartConfig(location="C:/javaee/git/JavaWeb/WebContent/upload2")
+@MultipartConfig(location="C:/javaee/git/JavaWeb/WebContent/upload3")
 @WebServlet("/Brad09")
 public class Brad09 extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=utf-8");
 		PrintWriter out = response.getWriter();
+
+		Collection<Part> parts= request.getParts();
+		for (Part part : parts){
+			String header = part.getHeader("Content-Disposition"); //Content-Disposition
+			
+			String myfilename = getFilename(header);
+			long size = part.getSize();
+			
+			out.print(myfilename + ":" + size + "<br>");
+			if (size>0){
+				part.write(myfilename);
+			}
+		}
 		
-		// must be @MultipartConfig => Tomcat
-		Part part = request.getPart("upload");
-		
-		String header = part.getHeader("Content-Disposition"); //Content-Disposition
-		String myfilename = getFilename(header);
-		
-		part.write(myfilename);
 	}
 	
 	private String getFilename(String body){
