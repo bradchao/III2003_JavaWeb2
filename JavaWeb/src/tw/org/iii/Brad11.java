09716813a7d365c0f5b835b1db34b900fb55094e
user <user@Z97>
2016-12-03 15:25:30 +0800

    201612031525

---
+++ JavaWeb/src/tw/org/iii/Brad11.java
@@ -0,0 +1,14 @@
+package tw.org.iii;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Brad11")
+public class Brad11 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+}
